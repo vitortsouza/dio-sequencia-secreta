723b5677f318676e4580cd2ecdc5d805dc6900d6
vitortsouza <129435933+vitortsouza@users.noreply.github.com>
2023-04-17 00:02:31 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program 
+{
+  public static void Main() 
+  {
+    int tamanhoSequencia = Convert.ToInt32(Console.ReadLine());
+    int quantidadeNumerosMarcados = 0;
+    int ultimoNumeroLido = 0; // Inicializa em zero, porque "Vi é igual a 1 ou 2";
+    for (int i = 0; i < tamanhoSequencia; i++) 
+    {
+      int vi = Convert.ToInt32(Console.ReadLine());
+      //Verificar se o numero lido (Vi) é diferente do último numero lido para marcá-lo.
+      if (vi != ultimoNumeroLido)
+      {
+        quantidadeNumerosMarcados += 1;
+      }
+      ultimoNumeroLido = vi;
+    }
+    //Imprimir a quantidade de números marcados.
+    Console.WriteLine(quantidadeNumerosMarcados);
+  }
+}
